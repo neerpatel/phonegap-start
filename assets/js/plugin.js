36ca69883ef385740bb1f0ddf53561ab11d4b89c
neerpatel <neerpatel@gmail.com>
2013-04-16 15:06:36 -0700

    commit from session

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -1,21 +1,15 @@
-        function TweetUpdate(){
+       function TweetUpdate(){
         var html = '<ul class="tweet_list">';
-        var tweeturl = "http://nullpointers.com/twitter.json.php?q=Texas Rangers";
-        console.log( "start" );
+        var tweeturl = "http://nullpointers.com/twitter.json.php?q=MultiValue";
         $.getJSON(tweeturl, function(d) {
-            console.log (d);
             $.each(d.statuses, function(i, tweet) {
-                console.log (tweet);
                 if(tweet.text !== undefined) {
                     // Calculate how many hours ago was the tweet posted
                     html += '<li>';
-        
                     html    += ' <a href="http://twitter.com/' + tweet.user.screen_name + '"> ' + tweet.user.screen_name + '</a> - ';
                     html    += tweet.text + '</li>';    
                 }
-					
             });
-
             html += "</ul>";
             $('#twitter_feed').append(html);
         })
@@ -27,10 +21,55 @@
 
  $(function() {
             // Bind the swipeleftHandler callback function to the swipe event on div.box
-            $( "#twitter_feed" ).on( 'swipeleft', swipeleftHandler );
+            $( "div.box" ).on( 'swipeleft', swipeleftHandler );
  
             // Callback function references the event target and adds the 'swipe' class to it
             function swipeleftHandler( event ) {
-                    $(event.target).css( "background-color","red" );
+                    $(event.target).addClass( "swipeleft" );
                 }
       });
+
+
+$(document ).on("pageinit","#gps-map", function(event) {
+                var mapdata = { destination: new google.maps.LatLng(32.7509, -97.3287) };
+                 if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(function(position) {
+                           
+                                var mapdata = { destination: new google.maps.LatLng(position.coords.latitude, position.coords.longitude) };
+                                $('#map_square').gmap(
+                    { 'center' : mapdata.destination, 
+                      'zoom' : 12, 
+                      'mapTypeControl' : false, 
+                      'navigationControl' : false,
+                      'streetViewControl' : false 
+                    })
+                    .bind('init', function(evt, map) { 
+                        $('#map_square').gmap('addMarker', 
+                            { 'position': map.getCenter(), 
+                              'animation' : google.maps.Animation.DROP
+             });                                                                                                                                                                                                               
+                    });
+                        });
+                    } else {
+                        //error('not supported');
+                        var mapdata = { destination: new google.maps.LatLng(32.7509, -97.3287) };
+                        $('#map_square').gmap(
+                    { 'center' : mapdata.destination, 
+                      'zoom' : 12, 
+                      'mapTypeControl' : false, 
+                      'navigationControl' : false,
+                      'streetViewControl' : false 
+                    })
+                    .bind('init', function(evt, map) { 
+                        $('#map_square').gmap('addMarker', 
+                            { 'position': map.getCenter(), 
+                              'animation' : google.maps.Animation.DROP
+             });                                                                                                                                                                                                               
+                    });
+                    }
+                
+                $('#map_square').click( function() { 
+                    $.mobile.changePage($('#gps-map'), {});
+                });              
+
+            });
